bdd257bb4447799f8ae627433605bf3827665856
Gilberto Silva <agilberto20111@hotmail.com>
2020-04-08 19:02:16 -0300

    Declaração de tipos publico e privado

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,9 +4,11 @@ import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
 
-	/*Main é um metodo auto executavel em java*/
+	 
 	public static void main(String[] args) {
 		
+		//new Aluno() é uma instancia (criação de um objeto
+		//aluno1 é uma referência para o objeto aluno
 		Aluno aluno1 = new Aluno(); //Aqui será o João
 		Aluno aluno2 = new Aluno(); //Aqui será o Pedro
 		Aluno aluno3 = new Aluno(); //Aqui seré o Alex

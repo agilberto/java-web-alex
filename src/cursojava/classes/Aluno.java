214a50aabaa7901d0000fcacd22d5722407fb018
Gilberto Silva <agilberto20111@hotmail.com>
2020-04-08 15:14:05 -0300

    Primeira classe e Objeto Aluno

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -1,5 +1,31 @@
 package cursojava.classes;
 
 public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGral;
+	String cpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscolaa;
+	String serieMatriculado;
+	
+	public Aluno() {
+	}
+	
+	public Aluno(String nomePadrao) {
+		this.nome=nomePadrao;
+	}
+	
+	public Aluno(String nomePadrao, int idadePadrao) {
+		this.nome=nomePadrao;
+		this.idade=idadePadrao;
+	}
+	
+	
+	
 
 }
---
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,21 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class PrimeiraClasseJava {
+
+	/*Main é um metodo auto executavel em java*/
+	public static void main(String[] args) {
+		
+		Aluno aluno1 = new Aluno(); //Aqui será o João
+		Aluno aluno2 = new Aluno(); //Aqui será o Pedro
+		Aluno aluno3 = new Aluno(); //Aqui seré o Alex
+		Aluno aluno4 = new Aluno("Maria"); 
+		Aluno aluno5 = new Aluno("José",50); 
+		 
+	
+		
+		
+	}
+
+}
